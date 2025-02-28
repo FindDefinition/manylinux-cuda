fdab233398a53d67a948fd56bd6056750024f4ab
yan.yan <yanyan.sub@outlook.com>
2025-02-28 15:59:18 +0800

    change almalinux gcc

---
+++ scripts/debug_build.sh
@@ -0,0 +1,2 @@
+docker build manylinux/cu126 --file ./manylinux/cu126/base.Dockerfile --tag manylinux2014-cuda:cu126-runtime
+# docker build manylinux/cu126 --file ./manylinux/cu126/devel.Dockerfile --tag manylinux2014-cuda:cu126-devel
